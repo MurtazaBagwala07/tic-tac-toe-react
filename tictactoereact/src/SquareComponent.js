a3ee1d09a1a550fdcf6beea7dc3bdc2131ceb20c
MurtazaBagwala07 <murtazabag786@gmail.com>
2021-09-11 01:48:22 +0530

    buggy app done

---
+++ tictactoereact/src/SquareComponent.js
@@ -0,0 +1,13 @@
+
+
+function SquareComponent(props) {
+    const classes =props.className?`${props.className} square`:'square';
+    return (
+      <span className={classes} onClick={props.onClick}>
+        {props.state}
+      </span>
+    );
+  }
+  
+  export default SquareComponent;
+  
