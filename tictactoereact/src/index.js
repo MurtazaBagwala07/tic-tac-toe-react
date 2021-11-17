781384aa6db2b58d8b61936c61ad55bb90b15212
MurtazaBagwala07 <murtazabag786@gmail.com>
2021-11-17 23:00:48 +0530

    Router started

--- tictactoereact/src/index.js
+++ tictactoereact/src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {BrowserRouter as Router,Routes,Route} from 'react-router-dom';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Router>
+    <Routes>
+      <Route path="/" element={<App/>}/>
+    </Routes>
+  </Router>,
   document.getElementById('root')
 );
 
