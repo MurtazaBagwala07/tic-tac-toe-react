d6599afab329df304130c1146baa7fa318eeca29
MurtazaBagwala07 <murtazabag786@gmail.com>
2021-11-18 21:56:58 +0530

    TEST

--- tictactoereact/src/Home.js
+++ tictactoereact/src/Home.js
@@ -13,10 +13,8 @@ function Home() {
     return (
         <div>
             <input type="text" />
-            <button onClick={setPLayerName(one)}>Set Player 1 Name</button>
-
             <input type="text" />
-            <button onClick={setPLayerName(two)}>Set Player 2 Name</button>
+            <button>Set Player 2 Name</button>
         </div>
     )
 }
--- tictactoereact/src/index.js
+++ tictactoereact/src/index.js
@@ -4,11 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter as Router,Routes,Route} from 'react-router-dom';
+import Home from './Home';
 
 ReactDOM.render(
   <Router>
     <Routes>
-      <Route path="/" element={<App/>}/>
+      <Route path="/" element={<Home/>}/>
     </Routes>
   </Router>,
   document.getElementById('root')
